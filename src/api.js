ec67630cbdbcf44884d4b55fd21d8a283300ceac
kboranbayev <boranbayev.k@gmail.com>
2020-08-11 03:10:48 -0700

    packages are updated

--- src/api.js
+++ src/api.js
@@ -3,17 +3,17 @@ import axios from "axios";
 export default {
   user: {
     login: credentials =>
-      axios.post("/api/auth", { credentials }).then(res => res.data.user),
+      axios.post("/auth", { credentials }).then(res => res.data.user),
     signup: user =>
-      axios.post("/api/users", { user }).then(res => res.data.user),
+      axios.post("/users", { user }).then(res => res.data.user),
     confirm: token =>
       axios
-        .post("/api/auth/confirmation", { token })
+        .post("/auth/confirmation", { token })
         .then(res => res.data.user),
     resetPasswordRequest: email =>
-      axios.post("/api/auth/reset_password_request", { email }),
-    validateToken: token => axios.post("/api/auth/validate_token", { token }),
-    resetPassword: data => axios.post("/api/auth/reset_password", { data }),
-    sendMessage: data => axios.post("/api/auth/send_message", { data }).then(res => res.data)
+      axios.post("/auth/reset_password_request", { email }),
+    validateToken: token => axios.post("/auth/validate_token", { token }),
+    resetPassword: data => axios.post("/auth/reset_password", { data }),
+    sendMessage: data => axios.post("/auth/send_message", { data }).then(res => res.data)
   }
 };
